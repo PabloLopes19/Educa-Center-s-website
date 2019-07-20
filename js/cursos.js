d246784ad123d4bbd5cff85c4a37380380863108
Pablo Lopes <46327427+PabloLopes19@users.noreply.github.com>
2019-07-20 01:27:01 -0300

    Update cursos.js

--- js/cursos.js
+++ js/cursos.js
@@ -2,7 +2,7 @@ function programming(){
     window.location.href = "programacao.html";
 }
 function webDesign(){
-    window.location.href = "WebDesign.html";
+    window.location.href = "photoshop.html";
 }
 tecFundament = () => {
     window.location.href = "tecnolo.html";
@@ -15,4 +15,28 @@ melhorIdade = () => {
 }
 vendaTecni = () => {
     window.location.href = "tecnicaVendas.html";
+}
+credECobr = () => {
+    window.location.href = "crediECobr.html";
+}
+contabeis = () => {
+    window.location.href = "contabeis.html";
+}
+veterinario = () => {
+    window.location.href = "veterinario.html";
+}
+hardware = () => {
+    window.location.href = "hardware.html";
+}
+robotica = () => {
+    window.location.href = "robotica.html";
+}
+ingles = () => {
+    window.location.href = "ingles.html";
+}
+farmacia = () => {
+    window.location.href = "farmacia.html";
+}
+jogos = () => {
+    window.location.href = "jogos.html";
 }
