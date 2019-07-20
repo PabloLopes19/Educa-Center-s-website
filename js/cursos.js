0ab6ae02f9578c3b20003f590d9e37086923d9bd
Pablo Lopes <46327427+PabloLopes19@users.noreply.github.com>
2019-07-20 00:32:23 -0300

    Add files via upload

---
+++ js/cursos.js
@@ -0,0 +1,18 @@
+function programming(){
+    window.location.href = "programacao.html";
+}
+function webDesign(){
+    window.location.href = "WebDesign.html";
+}
+tecFundament = () => {
+    window.location.href = "tecnolo.html";
+}
+projDesenhos = () => {
+    window.location.href = "projDesenhos.html";
+}
+melhorIdade = () => {
+    window.location.href = "melhorIdade.html";
+}
+vendaTecni = () => {
+    window.location.href = "tecnicaVendas.html";
+}
