499e2b2fc24063d1e42c8f9a2950487122e9cb4e
MiyamuraH <51636912+MiyamuraH@users.noreply.github.com>
2019-08-06 15:53:14 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,37 @@
+function redirectPrincipal(){
+    window.location.href = "principal.html";
+}
+function redirectCursos(){
+    window.location.href = "cursos.html";
+}
+function redirectSobre(){
+    window.location.href = "sobre.html";
+}
+function redirectContato(){
+    window.location.href = "contatos.html";
+}
+function redirectFinanceiro(){
+    window.location.href = "financeiro.html";
+}
+function easterEgg(){
+    window.location.href = "credits.html";
+}
+function loop(){
+    alert("Você já está nessa aba");
+}
+
+function facebook(){
+    window.open('https://www.facebook.com/grupoeducacenter', '_blank');
+}
+function insta(){
+    window.open('https://www.instagram.com/grupoeducacenter/', '_blank');
+}
+function gitPablo(){
+    window.open('https://github.com/PabloLopes19', '_blank');
+}
+function gitCarlos(){
+    window.open('https://www.instagram.com/grupoeducacenter/', '_blank');
+}
+testeMarlon = () => {
+    window.location.href = "programacao.html";
+}
